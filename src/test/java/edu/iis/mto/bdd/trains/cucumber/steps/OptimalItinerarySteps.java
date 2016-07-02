192dcd77ebb9ac277ae64309a255b7ab961e862a
bulikd105 <bulikd105@gmail.com>
2016-07-02 19:05:08 +0200

    Poprawienie klasy OptimalItinerarySteps

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -4,28 +4,42 @@ import java.util.List;
 
 import org.joda.time.LocalTime;
 
-import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 
-public class OptimalItinerarySteps {
+public class OptimalItinerarySteps 
+{
+    private static String line;
+    private static String lineStart;
+    private static String departure;
+    private static String destination;
+    private static List<LocalTime> departureTimes;
 
-	@Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
-	public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
-			@Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-		throw new PendingException();
+    InMemoryTimetableService timetableService = new InMemoryTimetableService();
 
-	}
+    @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
+    public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
+                                    @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) 
+    {
+        this.line = line;
+        this.lineStart = lineStart;
+        this.departure = departure;
+        this.destination = destination;
+        this.departureTimes = departureTimes;
+    }
 
-	@Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
-	public void whenIWantToTravel(String departure, String destination, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-		throw new PendingException();
-	}
+    @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
+    public void whenIWantToTravel(String departure, String destination, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) 
+    {
 
-	@Wtedy("^powinienem uzyskać informację o pociągach o:$")
-	public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-		throw new PendingException();
-	}
+    }
+
+    @Wtedy("^powinienem uzyskać informację o pociągach o:$")
+    public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) 
+    {
+
+    }
 }
